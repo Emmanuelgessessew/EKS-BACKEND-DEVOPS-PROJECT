48ec00351425b38e0b04f3a37f5bf518867568f5
emmanuel <emmanuelgessessew@gmail.com>
2026-03-27 18:17:55 +0000

    trigger ci cd

---
+++ terraform/eks.tf
@@ -0,0 +1,19 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0.0"
+
+  cluster_name    = "nextwork-eks-cluster"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
+    }
+  }
+}
